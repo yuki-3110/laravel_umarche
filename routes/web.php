d85c3b0914b3896cf057a52394ff6ee45e283ca8
yuki saito <ykst0622@gmail.com>
2024-01-10 01:49:18 +0900

    component準備

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComponentTestController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +30,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
+Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 
 require __DIR__.'/auth.php';
