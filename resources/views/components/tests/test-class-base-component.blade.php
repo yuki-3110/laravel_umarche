df078b81ba6c6e6b42b77f7a5a85c5c1c588ffd1
yuki saito <ykst0622@gmail.com>
2024-01-13 03:06:12 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+   クラスベースのコンポーネントです
+   <div>{{$classBaseMessage}}</div>
+   <div>{{$defaultMessage}}</div>
+    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
+</div>
