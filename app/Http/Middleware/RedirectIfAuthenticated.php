14676cb0d4a307cc33eae4bcfa1fa06387815476
yuki saito <ykst0622@gmail.com>
2024-01-18 02:30:19 +0900

    multilogin

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -9,6 +9,9 @@ use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
+    private const GUARD_USER = 'users';
+    private const GUARD_OWNER = 'owners';
+    private const GUARD_ADMIN = 'admin';
     /**
      * Handle an incoming request.
      *
@@ -21,10 +24,23 @@ class RedirectIfAuthenticated
     {
         $guards = empty($guards) ? [null] : $guards;
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+        // foreach ($guards as $guard) {
+        //     if (Auth::guard($guard)->check()) {
+        //         return redirect(RouteServiceProvider::HOME);
+        //     }
+        // }
+
+        //ユーザーとして認証されているかどうか、かつリクエストがユーザー関連のURLかどうか
+        if(Auth::guard(self::GUARD_USER)->check() && $request->routeIs('user.*')){
+            return redirect(RouteServiceProvider::HOME);
+        }
+
+        if(Auth::guard(self::GUARD_OWNER)->check() && $request->routeIs('owner.*')){
+            return redirect(RouteServiceProvider::OWNER_HOME);
+        }
+
+        if(Auth::guard(self::GUARD_ADMIN)->check() && $request->routeIs('admin.*')){
+            return redirect(RouteServiceProvider::ADMIN_HOME);
         }
 
         return $next($request);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,13 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+            @if(auth('admin')->user())
+                @include('layouts.admin-navigation')
+            @elseif(auth('owners')->user())
+                @include('layouts.owner-navigation')
+            @elseif(auth('users')->user())
+                @include('layouts.user-navigation')
+            @endif
 
             <!-- Page Heading -->
             @if (isset($header))
